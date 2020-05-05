bdf5c1e8a78d07460ea9d042635ceae921980c61
gality860702 <63327350+gality860702@users.noreply.github.com>
2020-05-05 14:31:19 +0800

    unityclass3-mon

---
+++ unity_monster0505/Assets/Bat.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bat : MonoBehaviour
+{
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("史萊姆")]
+    public Slima slime;
+
+    private float hp = 120;
+    private float atk = 20;
+    private float cure = 15;
+
+    ///<summary>
+    ///攻擊
+    /// </summary>
+    public void Attack()
+    {
+        slime.Damage(atk);
+    }
+    ///<summary>
+    ///受傷
+    /// </summary>
+    /// <param name="damage">要接收的傷害值</param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        message.text = gameObject.name + "-受到傷害:" + damage;
+        message.text +="\n" +gameObject.name + "-血量剩下" + hp;
+    }
+
+    ///<summary>
+    ///治療
+    /// </summary>
+    public void Cure()
+    {
+        hp += cure;
+        message.text = gameObject.name + "受到治療" + cure;
+        message.text+= "\n" + gameObject.name + "血量剩下" + hp;
+
+
+    }
+}
---
+++ unity_monster0505/Assets/Slima.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slima : MonoBehaviour
+{
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("蝙蝠")]
+    public Bat bat;
+
+    private float hp = 180;
+    private float atk = 10;
+    private float cure = 10;
+
+    ///<summary>
+    ///攻擊
+    /// </summary>
+    public void Attack()
+    {
+        bat.Damage(atk);
+    }
+    ///<summary>
+    ///受傷
+    /// </summary>
+    /// <param name="damage">要接收的傷害值</param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        message.text = gameObject.name + "-受到傷害:" + damage;
+        message.text += "\n" + gameObject.name + "-血量剩下" + hp;
+    }
+    ///<summary>
+    ///治療
+    /// </summary>
+    public void Cure()
+    {
+        hp += cure;
+        message.text = gameObject.name + "受到治療" + cure;
+        message.text += "\n" + gameObject.name + "血量剩下" + hp;
+
+
+    }
+
+
+}
